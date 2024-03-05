511b2c94fbf5e8dcb024a9f88c63bcb632e1edaf
NicteAC <carola.aliaga@gmail.com>
2024-03-05 20:59:38 -0300

    init

---
+++ js/assets/js/index.js
@@ -0,0 +1,15 @@
+/* var numero1 = 5;
+
+var numero2 = "5";
+
+//numero1 == numero2
+
+console.log(numero1 == numero2)
+
+var mascota = prompt("tiene usted mascotas?", "Si")
+
+document.write("Usted "+mascota+" posee mascotas") */
+
+
+var nombre = parseFloat(prompt("Ingrese su nombre"))
+console.log(typeof nombre)
